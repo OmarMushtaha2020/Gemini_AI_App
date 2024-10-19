8cb686f01022ea63d0df6efd31dbbfd663c7d4d9
omarmushtaha2525gmail.com <omarmushtaha37@gmail.com>
2024-10-19 13:57:07 +0300

    update of the ui MessageWidget

--- lib/message_widget.dart
+++ lib/message_widget.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class MessageWidget extends StatelessWidget {
   const MessageWidget({super.key,required this.text,required this.isFromUser});
@@ -11,10 +12,11 @@ final String  text;
     return Row(
       children: [
         Flexible(child: Container(constraints: BoxConstraints(maxWidth: 520),decoration: BoxDecoration(
-
+color: isFromUser?Theme.of(context).colorScheme.primary:Theme.of(context).colorScheme.secondary
+    ,borderRadius: BorderRadius.circular(18)
         ),child: Column(
           children: [
-            MarkdownBody(date)
+            MarkdownBody( data: text,)
           ],
         ),))
       ],
