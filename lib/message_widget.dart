344da53a21a41c40e046a9ad225edde1080bff38
omarmushtaha2525gmail.com <omarmushtaha37@gmail.com>
2024-10-19 13:55:06 +0300

    add flutter_markdown in the project

--- lib/message_widget.dart
+++ lib/message_widget.dart
@@ -1,16 +1,23 @@
 
 import 'package:flutter/material.dart';
 
-class MessageWidget extends StatefulWidget {
-  const MessageWidget({super.key});
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({super.key,required this.text,required this.isFromUser});
+final String  text;
+  final bool  isFromUser;
 
-  @override
-  State<MessageWidget> createState() => _MessageWidgetState();
-}
-
-class _MessageWidgetState extends State<MessageWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Flexible(child: Container(constraints: BoxConstraints(maxWidth: 520),decoration: BoxDecoration(
+
+        ),child: Column(
+          children: [
+            MarkdownBody(date)
+          ],
+        ),))
+      ],
+    );
   }
 }
