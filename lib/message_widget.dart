cad8bbef8a59f68ab456e7a2a6a121360056e601
omarmushtaha2525gmail.com <omarmushtaha37@gmail.com>
2024-10-19 13:47:48 +0300

    build MessageWidget

---
+++ lib/message_widget.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class MessageWidget extends StatefulWidget {
+  const MessageWidget({super.key});
+
+  @override
+  State<MessageWidget> createState() => _MessageWidgetState();
+}
+
+class _MessageWidgetState extends State<MessageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
