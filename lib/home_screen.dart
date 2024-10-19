a0d0f01ce1d3792d88d8d84a63064960a50744ca
omarmushtaha2525gmail.com <omarmushtaha37@gmail.com>
2024-10-19 13:43:36 +0300

    initialize  GenerativeModel and  ChatSession

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,27 @@
-
 import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late final GenerativeModel generativeModel;
+  late final ChatSession chatSession;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    generativeModel = GenerativeModel(
+        model: "gemini-pro",
+        apiKey:
+            String.fromEnvironment("AIzaSyBgV695z-ckb6s23P0pDebgBOzCAkmrZ2Q"));
+    chatSession=generativeModel.startChat();
+  }
 
   @override
   Widget build(BuildContext context) {
